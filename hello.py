8355bafd0d02a459f7d58cfe210b86a5efee7b2e
4nd3rsongeo <104532829+4nd3rsongeo@users.noreply.github.com>
2024-01-03 21:03:24 -0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,27 @@
+
+
+def funcao1():
+    # Código da função 1
+    print("Executando a função 1")
+
+def funcao2():
+    # Código da função 2
+    print("Executando a função 2")
+
+def main():
+    # Código principal ou lógica principal do programa
+    print("Iniciando o programa")
+    
+    # Chamando as funções desejadas
+    funcao1()
+    funcao2()
+    
+    print("Programa concluído")
+
+# Verifica se o script está sendo executado como o programa principal
+if __name__ == "__main__":
+    main()
+
+
+
+
